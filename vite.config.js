51683b81cbc7011cae42940bf9185ab9cc9af421
DhruviMevada <dhruvimrvada21@gmail.com>
2025-03-17 13:25:07 +0530

    Fix: Correct asset paths for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Transition_Capstone/',
-})
+  base: '/Transition_Capstone/' // Change this to match your repo name
+});
